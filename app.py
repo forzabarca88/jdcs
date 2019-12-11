a7cd0a1889dd401c4ecc71c4fef3a3e9977effdf
Justin D'Cruze <forzabarca88@gmail.com>
2019-12-11 18:15:01 +1100

    wip changes

--- app.py
+++ app.py
@@ -3,32 +3,38 @@ from utils import placeholder_text, get_all_files_in_dir
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index.html')
 def home():
     return render_template('index.html', title="Home Page",
-                            description=placeholder_text(3))
+                           description=placeholder_text(3))
+
 
 @app.route('/blog')
 def blog():
     return render_template('blog.html', title="Blog Posts",
-                            description=placeholder_text(3))
+                           description=placeholder_text(3))
+
 
 @app.route('/data')
 def data():
     return render_template('data.html', title="Data",
-                            description=placeholder_text(4))
+                           description=placeholder_text(4))
+
 
 @app.route('/gallery')
 def gallery():
     images = get_all_files_in_dir(os_dir_join_args=['static', 'gallery'],
-                                                    file_filter='.jpg')
+                                  file_filter='.jpg')
     return render_template('gallery.html', title="Gallery", images=images,
-                            description=placeholder_text(2))
+                           description=placeholder_text(2))
+
 
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -4,12 +4,16 @@ PLACEHOLDER_TEXT = '''
 Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. 
 Duis mollis, est non commodo luctus laoreet rutrum faucibus dolor auctor.
 '''
+
+
 def placeholder_text(multiplier=1):
     return ''.join(PLACEHOLDER_TEXT for i in range(multiplier))
+
 
 def get_all_files_in_dir(os_dir_join_args, file_filter=None):
     dir = os.path.join(*os_dir_join_args)
     files = os.listdir(path=dir)
     if file_filter:
-        files = [os.path.join(dir, file) for file in files if file.find(file_filter) >= 0]
+        files = [os.path.join(dir, file)
+                 for file in files if file.find(file_filter) >= 0]
     return files
