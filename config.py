aebf67791d9040b4194df835baad09e2609051d5
Justin D'Cruze <forzabarca88@gmail.com>
2019-12-14 22:32:47 +1100

    update config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import secrets
 import os
 
+ADMIN_EMAIL_ADDRESS = os.environ.get('APP_ADMIN_EMAIL', None)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('APP_SECRET_KEY', secrets.token_urlsafe())
-    
+SQLALCHEMY_DATABASE_URI = os.environ.get('APP_DB_URL', 
+                        'sqlite:///{}'.format(os.path.join(BASE_DIR, 'app.db')))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
