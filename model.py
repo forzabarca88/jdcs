2e7c5b97dd42a36699e4381cff04ac4207358fa8
Justin D'Cruze <forzabarca88@gmail.com>
2019-12-14 22:32:14 +1100

    add file for DB model

---
+++ model.py
@@ -0,0 +1,20 @@
+from app import db
+
+
+class Email(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.String(128), nullable=False)
+    receiver = db.Column(db.String(128), nullable=False)
+    subject = db.Column(db.String(128))
+    body = db.Column(db.String(2048))
+    latest_action_date = db.Column(db.DateTime, nullable=False)
+    sent = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return 'ID: {} \nFrom: {} \nTo: {} \nSubject: {} \nSent?: {}'.format(
+            self.id,
+            self.sender,
+            self.receiver,
+            self.subject,
+            self.sent
+        )
